695798350b88ffd2ad4b22c676eebcbb231aeafb
kutei <50226240+kutei@users.noreply.github.com>
2024-08-04 17:56:47 +0900

    [change]エンコーダ関連追加

---
+++ src/enc_reciever.cpp
@@ -0,0 +1,115 @@
+#include "enc_reciever.hpp"
+
+bool EncReciever::begin(Stream *stream, bool invert_enc, bool invert_sw)
+{
+    if(stream == NULL) return false;
+    this->_stream = stream;
+
+    this->_sem = xSemaphoreCreateMutex();
+    if(this->_sem == NULL) return false;
+
+    this->_invert_enc = invert_enc;
+    this->_invert_sw = invert_sw;
+
+    return true;
+}
+
+bool EncReciever::parse()
+{
+    uint16_t err_cnt = 1;
+
+    while(this->_stream->available() > 0){
+        uint8_t buf = this->_stream->read();
+
+        // スタートバイトがエラーかチェック
+        if(this->_parse_counter == 0){
+            if(buf != 's' && buf != 'p' && buf != 'n'){
+                err_cnt++;
+                continue;
+            }
+        }
+
+        // 受信バッファに格納
+        this->_recieve_buf[this->_parse_counter] = buf;
+        this->_parse_counter++;
+
+        if(this->_parse_counter >= EncReciever::ENC_DATA_LENGTH){
+            this->_parse_counter = 0;
+
+            // 受信データをパース
+            if(this->_parse_from_buffer_once() == true){
+                err_cnt = 0;
+            }
+        }
+    }
+
+    return err_cnt;
+}
+
+int32_t EncReciever::get_angle()
+{
+    if(xSemaphoreTake(this->_sem, portMAX_DELAY) != pdTRUE) return 0;
+    int32_t ret = this->_angle;
+    xSemaphoreGive(this->_sem);
+
+    return ret;
+}
+bool EncReciever::is_initialized()
+{
+    if(xSemaphoreTake(this->_sem, portMAX_DELAY) != pdTRUE) return false;
+    bool ret = this->_is_initialized;
+    xSemaphoreGive(this->_sem);
+
+    return ret;
+}
+bool EncReciever::is_on_upper_side()
+{
+    if(xSemaphoreTake(this->_sem, portMAX_DELAY) != pdTRUE) return false;
+    bool ret = this->_is_on_upper_side;
+    xSemaphoreGive(this->_sem);
+
+    return ret;
+}
+
+bool EncReciever::_parse_from_buffer_once()
+{
+    int32_t agl = 0;
+    bool init = false;
+    bool side = false;
+
+    // ステータスをパース
+    if(this->_recieve_buf[0] == 's')
+    {
+        init = true;
+        side = false;
+    }
+    else if(this->_recieve_buf[0] == 'p')
+    {
+        init = false;
+        side = true;
+    }
+    else if(this->_recieve_buf[0] == 'n')
+    {
+        init = false;
+        side = true;
+    }
+
+    // 回転角をパース
+    agl = (int32_t)this->_recieve_buf[1] << 24;
+    agl |= (int32_t)this->_recieve_buf[2] << 16;
+    agl |= (int32_t)this->_recieve_buf[3] << 8;
+    agl |= (int32_t)this->_recieve_buf[4];
+
+    // 反転設定を反映
+    if(this->_invert_enc) agl *= -1;
+    if(this->_invert_enc) side = !side;
+
+    // 公開用変数に格納
+    if(xSemaphoreTake(this->_sem, portMAX_DELAY) != pdTRUE) return false;
+    this->_angle = agl;
+    this->_is_initialized = init;
+    this->_is_on_upper_side = side;
+    xSemaphoreGive(this->_sem);
+
+    return false;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,29 @@
 #include "task_controller.hpp"
 #include "pwm_out.hpp"
+#include "enc_reciever.hpp"
 
 #include <Sbus2Reciever.hpp>
 
 #include <Arduino.h>
 #include <M5Unified.h>
 
+
+/**********************************************************************
+ * Control parameters
+ *********************************************************************/
+#define ROLL_ATTAKING_THRESHOLD 1550
+
+
 /**********************************************************************
  * Global variables
  *********************************************************************/
 Sbus2Reciever sbus2;
+EncReciever enc_boom;
 PwmOutServo motor_boom;
 PwmOutServo motor_roll;
-std::array<RtosTaskConfigRawPtr, 2> task_configs;
+
+int lcd_bottom_rect_x1, lcd_bottom_rect_x2, lcd_bottom_rect_x3;
+int lcd_bottom_rect_y1, lcd_bottom_rect_y2;
 
 
 
@@ -22,22 +33,37 @@ std::array<RtosTaskConfigRawPtr, 2> task_configs;
 void task_parse_sbus2(void *param)
 {
     sbus2.parse();
-    motor_boom.out((sbus2.getChannel(0)/1024.0)-1.0);
-    motor_roll.out((sbus2.getChannel(0)/1024.0)-1.0);
+}
+void task_parse_enc(void *param)
+{
+    enc_boom.parse();
 }
 
 void task_draw_infomation(void *param)
 {
     Serial.printf(
         "1:%5d, 2:%5d, 3:%5d, 4:%5d, 5:%5d, 6:%5d, "
-        "7:%5d, 8:%5d, 9:%5d,10:%5d, fs:%1d, lf:%1d\r\n",
+        "7:%5d, 8:%5d, 9:%5d,10:%5d, fs:%1d, lf:%1d, enc:%d.%d.%d\r\n",
         sbus2.getChannel(0), sbus2.getChannel(1),
         sbus2.getChannel(2), sbus2.getChannel(3),
         sbus2.getChannel(4), sbus2.getChannel(5),
         sbus2.getChannel(6), sbus2.getChannel(7),
         sbus2.getChannel(8), sbus2.getChannel(9),
-        sbus2.isFailsafe(), sbus2.isLostframe()
+        sbus2.isFailsafe(), sbus2.isLostframe(),
+        enc_boom.is_initialized(), enc_boom.is_on_upper_side(), enc_boom.get_angle()
     );
+}
+
+// 制御ループ
+void task_control_loop(void *param)
+{
+    // rollはそのまま出力
+    int16_t roll = sbus2.getChannel(0);
+    float roll_std = (roll / 1024.0 - 1.0) * 1.5;
+    motor_roll.out(roll_std);
+
+    // rollでの攻撃判定
+    bool attacking = roll > ROLL_ATTAKING_THRESHOLD;
 }
 
 
@@ -66,10 +92,18 @@ void setup() {
         while(1);
     };
 
+    // エンコーダ用にSerial2を設定
+    Serial2.begin(921600, SERIAL_8N1, 8, -1);
+    if(enc_boom.begin(&Serial2) != true){
+        M5.Display.printf("enc_boom failed\n");
+        while(1);
+    }
+
     // モーター出力を初期化
     motor_boom.begin(38, 0);
     motor_roll.begin(39, 1);
 
+    std::array<RtosTaskConfigRawPtr, 4> task_configs;
     task_configs[0] = new RtosTaskConfig_typedef{
         "task_parse_sbus2",
         NULL,
@@ -79,16 +113,31 @@ void setup() {
         task_parse_sbus2
     };
     task_configs[1] = new RtosTaskConfig_typedef{
+        "task_parse_enc",
+        NULL,
+        pdTRUE,
+        pdMS_TO_TICKS(1),
+        pdMS_TO_TICKS(0),
+        task_parse_enc
+    };
+    task_configs[2] = new RtosTaskConfig_typedef{
         "task_draw_infomation",
         NULL,
         pdTRUE,
-        pdMS_TO_TICKS(100),
+        pdMS_TO_TICKS(200),
         pdMS_TO_TICKS(0),
         task_draw_infomation
     };
+    task_configs[3] = new RtosTaskConfig_typedef{
+        "task_control_loop",
+        NULL,
+        pdTRUE,
+        pdMS_TO_TICKS(10),
+        pdMS_TO_TICKS(0),
+        task_control_loop
+    };
 
     task_initialize(task_configs.data(), task_configs.size());
-    task_start(task_configs.data(), task_configs.size());
 
     vTaskDelay(pdMS_TO_TICKS(1000));
     M5.Display.print("5");
@@ -101,10 +150,27 @@ void setup() {
     vTaskDelay(pdMS_TO_TICKS(1000));
     M5.Display.print(" 1\n");
     vTaskDelay(pdMS_TO_TICKS(1000));
+
+    task_start(task_configs.data(), task_configs.size());
     M5.Display.print("started\n");
+
+    lcd_bottom_rect_x1 = M5.Lcd.width()*0.9;
+    lcd_bottom_rect_x2 = M5.Lcd.width()*0.95;
+    lcd_bottom_rect_x3 = M5.Lcd.width()*1.0;
+    lcd_bottom_rect_y1 = M5.Lcd.height()*0.95;
+    lcd_bottom_rect_y2 = M5.Lcd.height()*1.0;
+
 }
+
+
+
 
 void loop() {
     // nothing to do.
-    vTaskDelay(1000);
+    vTaskDelay(100);
+    M5.Display.fillRect(lcd_bottom_rect_x1, lcd_bottom_rect_y1, lcd_bottom_rect_x2, lcd_bottom_rect_y2, WHITE);
+    M5.Display.fillRect(lcd_bottom_rect_x2, lcd_bottom_rect_y1, lcd_bottom_rect_x3, lcd_bottom_rect_y2, BLACK);
+    vTaskDelay(100);
+    M5.Display.fillRect(lcd_bottom_rect_x1, lcd_bottom_rect_y1, lcd_bottom_rect_x2, lcd_bottom_rect_y2, BLACK);
+    M5.Display.fillRect(lcd_bottom_rect_x2, lcd_bottom_rect_y1, lcd_bottom_rect_x3, lcd_bottom_rect_y2, WHITE);
 }
